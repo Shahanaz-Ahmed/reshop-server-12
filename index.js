c334926aea4eecb5d3ecb923226ca4be392cb03d
ShahanazAhmedNishi <asahanaz207@gmail.com>
2022-11-26 16:02:46 +0600

    categories api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,11 +15,38 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+  try {
+    const bookCategoriesCollection = client
+      .db("ReShop")
+      .collection("BookCategories");
+    const booksCollection = client.db("ReShop").collection("Books");
+
+    app.get("/BookCategories", async (req, res) => {
+      const query = {};
+      const options = await bookCategoriesCollection.find(query).toArray();
+      res.send(options);
+    });
+    //category dhore api load korar api
+    app.get("/category/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { category_id: id };
+      const books = await booksCollection.find(query).toArray();
+      res.send(books);
+    });
+    //specific books er id
+    app.get("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: id };
+      const books = await booksCollection.findOne(query);
+      res.send(books);
+    });
+  } finally {
+  }
+}
+run().catch(console.log);
 
 app.get("/", async (req, res) => {
   res.send("Reshop server is running");
